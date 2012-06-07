f0439d903466a34bb5223c8e7601038ed9f3b411
Chris Schmich <schmch@gmail.com>
2012-06-07 07:11:17 -0700

    Adding additional tests for set, get, delete.

--- tests.rb
+++ tests.rb
@@ -14,6 +14,53 @@ class TestWard < Test::Unit::TestCase
   end
 
   def test_set
-    @ward.set(:domain => 'foo.com', :password => 'bar')
+    created = @ward.set(:domain => 'foo.com', :password => 'bar')
+    assert(created)
+  end
+
+  def test_set_get
+    password = 'bar'
+    @ward.set(:domain => 'foo.com', :password => password)
+    get = @ward.get(:domain => 'foo.com')
+    assert_equal(password, get)
+  end
+
+  def test_set_update
+    created = @ward.set(:domain => 'foo.com', :password => 'bar')
+    assert(created)
+    created = @ward.set(:domain => 'foo.com', :password => 'baz')
+    assert(!created)
+  end
+
+  def test_get_fail
+    password = @ward.get(:domain => 'foo.com')
+    assert_nil(password)
+  end
+
+  def test_set_delete
+    created = @ward.set(:domain => 'foo.com', :password => 'bar')
+    assert(created)
+    deleted = @ward.delete(:domain => 'foo.com')
+    assert(deleted)
+  end
+
+  def test_delete_fail
+    deleted = @ward.delete(:domain => 'foo.com')
+    assert(!deleted)
+  end
+
+  def test_set_delete_get_fail
+    password = 'bar'
+    created = @ward.set(:domain => 'foo.com', :password => password)
+    assert(created)
+    get = @ward.get(:domain => 'foo.com')
+    assert_equal(password, get)
+    deleted = @ward.delete(:domain => 'foo.com')
+    assert(deleted)
+    get = @ward.get(:domain => 'foo.com')
+    assert_nil(get)
+  end
+
+  def test_set_nick
   end
 end
