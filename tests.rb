0cacafdb7a1ccb8458c5691c1676fbc770824724
Chris Schmich <schmch@gmail.com>
2012-06-07 07:22:10 -0700

    Adding more tests, adding $dom/$pw shortcut variables.

--- tests.rb
+++ tests.rb
@@ -4,63 +4,93 @@ require 'tempfile'
 
 class TestWard < Test::Unit::TestCase
   def setup
-    password = 'test'
     @temp_store = Tempfile.new('ward')
-    @ward = Ward.new(@temp_store.path, password)
+    @master_password = 'test'
+    @ward = new_ward()
   end
 
   def teardown
     @temp_store.delete rescue nil
   end
 
+  def new_ward
+    Ward.new(@temp_store.path, @master_password)
+  end
+
   def test_set
-    created = @ward.set(:domain => 'foo.com', :password => 'bar')
+    created = @ward.set(:domain => $dom, :password => $pw)
     assert(created)
   end
 
   def test_set_get
-    password = 'bar'
-    @ward.set(:domain => 'foo.com', :password => password)
-    get = @ward.get(:domain => 'foo.com')
-    assert_equal(password, get)
+    @ward.set(:domain => $dom, :password => $pw)
+    get = @ward.get(:domain => $dom)
+    assert_equal(get, $pw)
   end
 
   def test_set_update
-    created = @ward.set(:domain => 'foo.com', :password => 'bar')
+    created = @ward.set(:domain => $dom, :password => $pw)
     assert(created)
-    created = @ward.set(:domain => 'foo.com', :password => 'baz')
+    created = @ward.set(:domain => $dom, :password => $pw)
     assert(!created)
   end
 
   def test_get_fail
-    password = @ward.get(:domain => 'foo.com')
+    password = @ward.get(:domain => $dom)
     assert_nil(password)
   end
 
   def test_set_delete
-    created = @ward.set(:domain => 'foo.com', :password => 'bar')
+    created = @ward.set(:domain => $dom, :password => $pw)
     assert(created)
-    deleted = @ward.delete(:domain => 'foo.com')
+    deleted = @ward.delete(:domain => $dom)
     assert(deleted)
   end
 
   def test_delete_fail
-    deleted = @ward.delete(:domain => 'foo.com')
+    deleted = @ward.delete(:domain => $dom)
     assert(!deleted)
   end
 
   def test_set_delete_get_fail
-    password = 'bar'
-    created = @ward.set(:domain => 'foo.com', :password => password)
+    created = @ward.set(:domain => $dom, :password => $pw)
     assert(created)
-    get = @ward.get(:domain => 'foo.com')
-    assert_equal(password, get)
-    deleted = @ward.delete(:domain => 'foo.com')
+    get = @ward.get(:domain => $dom)
+    assert_equal(get, $pw)
+    deleted = @ward.delete(:domain => $dom)
     assert(deleted)
-    get = @ward.get(:domain => 'foo.com')
+    get = @ward.get(:domain => $dom)
     assert_nil(get)
+  end
+
+  def test_set_reopen_get
+    @ward.set(:domain => $dom, :password => $pw)
+    @new_ward = new_ward()
+    get = @new_ward.get(:domain => $dom)
+    assert_equal(get, $pw)
   end
 
   def test_set_nick
   end
+
+  def test_get_nick
+  end
+
+  def test_delete_nick
+  end
+
+  def test_nick_unique
+  end
+
+  def test_set_alias
+  end
+
+  def test_get_alias
+  end
+
+  def test_delete_alias
+  end
+
+  $dom = 'foo.com'
+  $pw = 'bar'
 end
