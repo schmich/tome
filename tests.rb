686cb20ddb1995cdc31ada2fb591fa0d88ef63ac
Chris Schmich <schmch@gmail.com>
2012-06-07 17:46:22 -0700

    Adding more tests for nicknames and checking more for WardError in expected cases.

--- tests.rb
+++ tests.rb
@@ -19,6 +19,12 @@ class TestWard < Test::Unit::TestCase
     assert(created)
   end
 
+  def test_set_fail
+    assert_raise(WardError) {
+      @ward.set({})
+    }
+  end
+
   def test_set_get
     @ward.set($dph)
     get = @ward.get($dh)
@@ -33,8 +39,8 @@ class TestWard < Test::Unit::TestCase
   end
 
   def test_get_fail
-    password = @ward.get($dh)
-    assert_nil(password)
+    get = @ward.get($dh)
+    assert_nil(get)
   end
 
   def test_set_delete
@@ -63,46 +69,96 @@ class TestWard < Test::Unit::TestCase
   def test_many_set_get
     created = @ward.set($dph)
     assert(created)
-    created = @ward.set($d2p2h)
+    created = @ward.set($DPh)
     assert(created)
     get = @ward.get($dh)
     assert_equal($p, get)
-    get = @ward.get($d2h)
-    assert_equal($p2, get)
+    get = @ward.get($Dh)
+    assert_equal($P, get)
   end
 
   def test_set_nick
+    created = @ward.set($dpnh)
+    assert(created)
   end
 
   def test_get_nick
+    created = @ward.set($dpnh)
+    assert(created)
+    get = @ward.get($nh)
+    assert_equal($p, get)
+  end
+
+  def test_get_nick_fail
+    get = @ward.get($nh)
+    assert_nil(get)
   end
 
   def test_delete_nick
+    created = @ward.set($dpnh)
+    assert(created)
+    deleted = @ward.delete($nh)
+    assert(deleted)
+  end
+
+  def test_delete_nick_fail
+    deleted = @ward.delete($nh)
+    assert(!deleted)
+  end
+
+  def test_nick_update
+    created = @ward.set($dpnh)
+    assert(created)
+    created = @ward.set($Pnh)
+    assert(!created)
+    get = @ward.get($nh)
+    assert_equal($P, get)
+  end
+
+  def test_set_nick_fail
+    assert_raise(WardError) {
+      @ward.set($pnh)
+    }
   end
 
   def test_nick_unique
+    created = @ward.set($dpnh)
+    assert(created)
+    assert_raise(WardError) {
+      @ward.set($Dpnh)
+    }
   end
 
-  def test_set_alias
-  end
-
-  def test_get_alias
-  end
-
-  def test_delete_alias
+  def test_set_delete_set_nick
+    created = @ward.set($dpnh)
+    assert(created)
+    deleted = @ward.delete($nh)
+    assert(deleted)
+    created = @ward.set($Dpnh)
+    assert(created)
   end
 
   $d = 'foo.com'
   $p = 'bar'
+  $n = 'quux'
   $dh = { :domain => $d }
   $ph = { :password => $p }
+  $nh = { :nick => $n }
   $dph = $dh.merge($ph)
+  $dpnh = $dph.merge($nh)
+  $pnh = $ph.merge($nh)
 
-  $d2 = 'baz.com'
-  $d2h = { :domain => $d2 }
-  $p2 = 'quux'
-  $p2h = { :password => $p2 }
-  $d2p2h = $d2h.merge($p2h)
+  $D = 'baz.com'
+  $Dh = { :domain => $D }
+  $P = 'quux'
+  $Ph = { :password => $P }
+  $DPh = $Dh.merge($Ph)
+  $N = 'waldo'
+  $Nh = { :nick => $N }
+  $DPNh = $DPh.merge($Nh)
+
+  $Pnh = $Ph.merge($nh)
+  $Dpnh = $Dh.merge($pnh)
 end
 
 class TestCommand < Test::Unit::TestCase
@@ -115,10 +171,19 @@ class TestCommand < Test::Unit::TestCase
     @temp_store.delete rescue nil
   end
 
+  def cmd(*args)
+    WardCommand.run(@temp_store.path, args)
+  end
+
   def test_set
   end
 
-  def cmd(*args)
-    WardCommand.run(@temp_store.path, args)
+  def test_set_alias
+  end
+
+  def test_get_alias
+  end
+
+  def test_delete_alias
   end
 end
